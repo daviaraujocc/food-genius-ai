1a49133f76ad0f8e0815fa942f5251f702b3c716
Davi Araujo <davi.araujo13356@gmail.com>
2025-01-21 15:41:55 -0300

    feat(service.py): add device option to bentoml service

--- service.py
+++ service.py
@@ -1,9 +1,12 @@
 import bentoml
-from PIL import Image as PILImage
+from PIL import Image as PILImage, UnidentifiedImageError
 import torch
 import foodgenius.model_builder as model_builder
 from typing import Dict
 from timeit import default_timer as timer
+from bentoml.exceptions import BadInput
+import os
+
 
 # Load Class Names
 with open('class_names.txt', 'r') as file:
@@ -15,6 +18,10 @@ food_nonfood_class_names = ['food', 'non_food']
 @bentoml.service(name="foodgenius-service", resources={"cpu": "1", "memory": "512Mi"})
 class FoodGenius:
     def __init__(self):
+        device = os.getenv("BENTOML_DEVICE", "cpu")
+        self.device = torch.device(device)
+
+
         effnetb2_101_model, effnetb2_101_transforms = model_builder.create_effnetb2_model(num_classes=len(class_names))
         effnetb2_101_model.load_state_dict(torch.load(f='models/pretrained_effnetb2_food_101.pth', map_location='cpu'))
 
@@ -29,29 +36,38 @@ class FoodGenius:
 
     @bentoml.api
     def classify(self, img: PILImage.Image) -> Dict:
-    
         start_time = timer()
-    
-        img_transformed = self.effnetb2_food_nonfood_transforms(img).unsqueeze(0).to('cpu')
-    
-   
-        self.effnetb2_model_food_nonfood.eval()
-        with torch.inference_mode():
-            food_nonfood_pred_probs = torch.softmax(self.effnetb2_model_food_nonfood(img_transformed), dim=1)
-    
-        is_food = food_nonfood_pred_probs[0][0] > 0.5  # Assuming threshold of 0.5 for food
-    
-        if is_food:
-            img_transformed = self.effnetb2_101_transforms(img).unsqueeze(0).to('cpu')
-        
-            self.effnetb2_101_model.eval()
+
+        try:
+            # Check if the image format is supported
+            if img.format not in ["JPEG", "JPG", "PNG"]:
+                return {"error": "Unsupported image format. Please upload a JPG, JPEG, or PNG image."}
+
+            if img.mode == 'RGBA':
+                img = img.convert('RGB')
+
+            img_transformed = self.effnetb2_food_nonfood_transforms(img).unsqueeze(0).to(self.device)
+
+            self.effnetb2_model_food_nonfood.eval()
             with torch.inference_mode():
-                pred_probs = torch.softmax(self.effnetb2_101_model(img_transformed), dim=1)
-                pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
-        else:
-            pred_labels_and_probs = {'nonfood': float(food_nonfood_pred_probs[0][1])}
-    
-        pred_time = round(timer() - start_time, 5)
-    
-        return {"predictions": pred_labels_and_probs, "time_taken": pred_time, "top_prediction": max(pred_labels_and_probs, key=pred_labels_and_probs.get)}
+                food_nonfood_pred_probs = torch.softmax(self.effnetb2_model_food_nonfood(img_transformed), dim=1)
+
+            is_food = food_nonfood_pred_probs[0][0] > 0.5  # Assuming threshold of 0.5 for food
+
+            if is_food:
+                img_transformed = self.effnetb2_101_transforms(img).unsqueeze(0).to(self.device)
+
+                self.effnetb2_101_model.eval()
+                with torch.inference_mode():
+                    pred_probs = torch.softmax(self.effnetb2_101_model(img_transformed), dim=1)
+                    pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
+            else:
+                pred_labels_and_probs = {'nonfood': float(food_nonfood_pred_probs[0][1])}
+
+            pred_time = round(timer() - start_time, 5)
+
+            return {"predictions": pred_labels_and_probs, "time_taken": pred_time, "top_prediction": max(pred_labels_and_probs, key=pred_labels_and_probs.get)}
+
+        except BadInput:
+            return {"error": "Invalid input. Please upload a valid JPG, JPEG, or PNG image."}
     
